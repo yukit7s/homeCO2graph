56da659f36742861f9d86b881078c17bc024b563
yuki <46600832+yukit7s@users.noreply.github.com>
2021-10-02 21:03:23 +0900

    ログインページ作りかけ

---
+++ src/js/firebase/email.js
@@ -0,0 +1,24 @@
+firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ..
+    });
+
+
+firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    });
+
